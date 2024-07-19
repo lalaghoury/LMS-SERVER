97bf9ad3f93a73f29238fffde2a35699acf5827c
lalaghoury <lala.ghoury@gmail.com>
2024-07-19 18:59:15 +0500

    done ass submission and creation

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const useCors = (app) => {
   app.use(
     cors({
-      origin: ["http://localhost:3000", "*"],
+      origin: ["http://localhost:3000", "https://aasilghourydevlms-aasil-ghourys-projects.vercel.app"],
       credentials: true,
     })
   );
